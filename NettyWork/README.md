b6bc45d9fc0f65d6ea9f735be98bc9d626f625b4
wurenjie <wurenjie@atplatform.net>
2018-09-30 10:09:48 +0800

    learn netty

---
+++ NettyWork/README.md
@@ -0,0 +1,6 @@
+# NettyWork
+
+## Link
+- [Netty 源码分析之 三 我就是大名鼎鼎的 EventLoop(一)](https://segmentfault.com/a/1190000007403873)
+- [waylau / netty-4-user-guide-demos](https://github.com/waylau/netty-4-user-guide-demos)
+- [Netty 4.x User Guide 中文翻译《Netty 4.x 用户指南》](https://waylau.com/netty-4-user-guide/)
---
+++ NettyWork/src/main/java/demo/echo/EchoServer.java
@@ -0,0 +1,49 @@
+package demo.echo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class EchoServer {
+    private static final int PORT = 8090;
+
+    public void run(){
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new EchoServerHandler());
+                    }
+                })
+                .option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+        try {
+            ChannelFuture f = bootstrap.bind(PORT).sync();
+
+            System.out.println("server start");
+
+            f.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        new EchoServer().run();
+    }
+
+}
